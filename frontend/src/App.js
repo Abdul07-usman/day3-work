75eab4b8cca2abf7162d709f3eb39e4642f42111
Ramsoon <sadiqabdulrahman00880@gmail.com>
2025-10-14 19:16:06 +0100

    14-10-25

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Login App</h1>
+      <h1>NIMC Login App</h1>
       <form onSubmit={handleLogin}>
         <input
           type="text"
